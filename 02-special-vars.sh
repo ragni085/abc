2461bcdf3d3323842da8bf04849dc1d49db66bd0
Ragni kumari <ragni085@gmail.com>
2020-05-16 17:02:35 +0530

    adding new file

---
+++ 02-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+## Special variables are 0-n,  *, @, #
+
+echo Name of the Script = $0
+echo First Argument = $1
+echo Second Argument = $2
+
+echo All Arguments = $*
+echo All Arguments = $@
+
+echo Number of Arguments = $#
